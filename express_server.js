8b56cd7790a4cc647e31f7f003bd238b6a84f61e
Chris Bell <chrisbeckertonbell@gmail.com>
2022-01-24 15:26:22 -0800

    added ;'s to app.get('/urls)

--- express_server.js
+++ express_server.js
@@ -26,6 +26,11 @@ app.get('/hello', (req, res) => {
 });
 
 app.get('/urls', (req, res) => {
-  const templateVars = { urls: urlDatabase }
-  res.render('urls_index', templateVars)
+  const templateVars = { urls: urlDatabase };
+  res.render('urls_index', templateVars);
 });
+
+// app.get("/urls", (req, res) => {
+//   const templateVars = { urls: urlDatabase }
+//   res.render("urls_index", templateVars)
+// });
