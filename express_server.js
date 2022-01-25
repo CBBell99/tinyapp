51b9885e9261c618c54aca160d40eeb8419f4e79
Chris Bell <chrisbeckertonbell@gmail.com>
2022-01-24 18:40:15 -0800

    installed body parser as a dependency, implemented a random string generator

--- express_server.js
+++ express_server.js
@@ -4,6 +4,21 @@ const PORT = 8080;
 
 app.set('view engine', 'ejs');
 
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
+function generateRandomString() {
+  const characters = 'abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  const length = characters.length;
+  let result = '';
+  for (let i = 0; i < 6; i++) {
+    result += characters.charAt(Math.floor(Math.random() * length));
+  }
+  return result;
+}
+
 const urlDatabase = {
   'b2xVn2': 'http://www.lighthouselabs.ca',
   '9sm5xK': 'http://www.google.com'
@@ -24,13 +39,25 @@ app.get('/urls.json', (req, res) => {
 app.get('/hello', (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+//new url
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new');
+});
 
 app.get('/urls', (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render('urls_index', templateVars);
 });
 
+
+// short url
 app.get('/urls/:shortURL', (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL };
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render('urls_show', templateVars);
+});
+
+//post
+app.post('/urls', (req, res) => {
+  console.log(req.body);
+  res.send('OK');
 });
