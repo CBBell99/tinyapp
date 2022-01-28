76c5ebec93a8556405fd3180f4edb49cc3ef515e
Chris Bell <chrisbeckertonbell@gmail.com>
2022-01-28 08:36:56 -0800

    tested if findUserByEmail works

---
+++ helpers.js
@@ -0,0 +1,33 @@
+//generates random 6 character string to act as a URL
+const generateRandomString = () => {
+  const characters = 'abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  const length = characters.length;
+  let result = '';
+  for (let i = 0; i < 6; i++) {
+    result += characters.charAt(Math.floor(Math.random() * length));
+  }
+  return result;
+};
+
+//verifies if email is in database
+const getUserByEmail = (email, database) => {
+  for (const user in database) {
+    if (database[user].email === email) {
+      return database[user];
+    }
+  }
+  return undefined
+}
+
+//returns the URLs where the userID is equal to the current id
+const urlsForUser = (id, urlDatabase) => {
+  let userUrls = {};
+  for (const shortURL in urlDatabase) {
+    if (urlDatabase[shortURL].userID === id) {
+      userUrls[shortURL] = urlDatabase[shortURL];
+    }
+  }
+  return userUrls;
+};
+
+module.exports = { getUserByEmail, urlsForUser }
